de6b6634d303bd26d032e8fbd4a4e961c551db2b
Grazielle Alves <graziellemontenegro@hotmail.com>
2020-09-11 17:20:25 -0300

    Criando formularios de login e cadastro

---
+++ todoapp/auth/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from todoapp.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired(), Length(min=3)])
+    confirm_password = StringField(
+        'Confirm Password',
+        validators=[DataRequired(), EqualTo('password')]
+    )
+    submit = SubmitField('Sign Up')
+
+
+    def validade_username(self, username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('That username is taken. Plase choose a different one.')
+
+    def validade_email(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise ValidationError('That email is taken. Plase choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
